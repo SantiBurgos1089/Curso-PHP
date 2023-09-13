7f21b93298ef82865c5f803dc7ea7072e171611d
Santiago Burgos <santiago.burgos1089@gmail.com>
2023-09-13 10:47:14 -0600

    colocados archivos basicos para curso PHP

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title>Pruebas website</title>
+  </head>
+  <body>
+    <h1>Hello World!</h1>
+
+    <p>This is the landing page of <strong>Pruebas</strong>.</p>
+
+    <?php
+
+    // Mi primer hola mundo
+    echo "Hola mundo 1";
+
+    # Segundo hola mundo
+    echo "Hola mundo 2";
+
+    /*Comentario
+    multiples
+    lineas
+    pruebas
+    */
+    echo "Hola mundo 3";
+
+    ?>
+  </body>
+</html>
+
+
---
+++ tipos_datos.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+PHP no distingue
+entre las mayusculas y minusculas
+
+*/
+
+#1
+echo TRUE;
+
+#No se muestra, o es 0
+echo FALSE;
+
+echo -10;
+
+var_dump(TRUE);
+var_dump(-10);
+
+
+var_dump(-7.5);
+
+var_dump("esta es una cadena de texto")
+
+?>
