e2c9fe5dd96bf8e50db68547a17fba702587c605
Santiago Burgos <santiago.burgos1089@gmail.com>
2023-09-14 08:27:45 -0600

    añadido sitio variables php

---
+++ variables.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+1) Las variables en PHP se representan con un signo de dolar
+seguido por el nombre de la variable
+
+2) La variable tiene que comenzar con una letra o caracter de guion bajo,
+seguido de cualquier numero de letras, numeros y otros guiones bajos.
+No puede empezar con un numero
+
+3) El nombre de la variable es sensible a minusculas y mayusculas (es decir,
+"Variable" no es lo mismo que "variable" o "VARIABLE"). Se recomienda usar la
+convención Camel case (es decir, $Variable)
+
+*/
+
+$nombre="Santiago";
+echo $nombre;
+#echo $Nombre;
+
+$Nombre_completo = "Jose Santiago Burgos Mejia";
+echo $Nombre_completo;
