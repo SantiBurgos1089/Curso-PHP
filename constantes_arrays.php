0d9cc307c3818a6684dfad119369a1446988e463
Santiago Burgos <santiago.burgos1089@gmail.com>
2023-09-18 16:02:50 -0600

    Update constantes_arrays.php
    añadida información acerca del uso de arrays en PHP

--- constantes_arrays.php
+++ constantes_arrays.php
@@ -6,6 +6,42 @@ define("NOMBRE","Carlos");
 echo NOMBRE
 
 # Sintaxis nueva
-const NOMBRE1="Santiago"
+const NOMBRE1="Santiago";
 
 echo NOMBRE1
+
+# Arrays
+# Sintaxis basica de creacion y acceso
+$estudiantes=array("Carlos","Jose","Vanessa","Katy");
+echo $estudiantes[2];
+
+# Otra sintaxis adicional PHP >= 5.4
+$estudiantes1=["Carlos","Jose","Vanessa","Katy"];
+
+# Un array puede llevar varios tipos de valores
+$array1=["Nombre",7.5,2,"Cadena"];
+
+# Array de tipo asociativo
+# Su sintaxis suele ocuparse principalmente de la siguiente manera
+$tutor=[
+    "nombre"=>"Carlos",
+    "apellido"=>"Alfaro",
+    "edad"=>27
+];
+
+$tutor["edad"]=19
+echo $tutor["edad"];
+
+# Array de multiples dimensiones
+$tutor_2=[
+    "nombre"=>"Vanessa",
+    "apellido"=>"Doofensmirthz",
+    "edad"=>16,
+    "cursos"=>["PHP","Python","CSS"]
+];
+
+echo $tutor_2["cursos"][1];
+
+echo count($estudiantes)
+
+echo count($tutor_2,COUNT_RECURSIVE);
