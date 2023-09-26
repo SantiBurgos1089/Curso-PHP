f894a86e7ae5dc79e8367e4c9e8fe4f5670cb80d
Santiago Burgos <santiago.burgos1089@gmail.com>
2023-09-26 11:39:58 -0600

    Create operadores_aritmeticos.php
    creado archivo para ver funcionamiento de operadores aritmeticos

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,28 @@
+<?php
+
+$numero_1=2;
+$numero_2=5;
+
+# Suma
+$resultado_1=$numero_1+$numero_2;
+echo $resultado_1;
+
+# Resta
+$resultado_2=$numero_2-$numero_1;
+echo $resultado_2;
+
+# Multiplicacion
+$resultado_3=$numero_1*$numero_2;
+echo $resultado_3;
+
+# Division
+$resultado_4=100/$numero_2;
+echo $resultado_4;
+
+# Potencia/Exponenciación
+$resultado_5=$numero_2**$numero_1;
+echo $resultado_5;
+
+# Residuo/Division módulo
+$resultado_6=10%3;
+echo $resultado_6;
